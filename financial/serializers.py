dfc123172db5ffe284afb78c0ad5fc088d4b0d4c
Sadeghizad <mosadeghizad@gmail.com>
2024-10-08 16:50:32 +0330

    add a payment history serializers

--- financial/serializers.py
+++ financial/serializers.py
@@ -1,32 +1,59 @@
 from rest_framework import serializers
 from .models import *
 
-class PaymentSerializer(serializers.ModelSerializer):
+
+class InitiatePaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
-        fields = ['id', 'user', 'amount', 'transaction_id', 'payment_status', 'order_id', 'created_at']
+        fields = ["id", "subscriptionplan", "transaction_id", "created_at"]
+        read_only_fields = ["id", "transaction_id", "created_at"]
+
+
 class RefundSerializer(serializers.ModelSerializer):
     class Meta:
         model = Refund
-        fields = ['id', 'payment', 'refund_id', 'amount', 'refund_status', 'created_at']
+        fields = ["id", "payment", "refund_id", "amount", "refund_status", "created_at"]
+
 
 class SubscriptionPlanSerializer(serializers.ModelSerializer):
     class Meta:
         model = SubscriptionPlan
         fields = "__all__"
+
+
 class InitiatePaymentSerializer(serializers.ModelSerializer):
-    subscriptionplan = serializers.PrimaryKeyRelatedField(queryset=SubscriptionPlan.objects.all())
+    subscriptionplan = serializers.PrimaryKeyRelatedField(
+        queryset=SubscriptionPlan.objects.all()
+    )
     amount = serializers.DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
         model = Payment
-        fields = ['subscriptionplan', 'amount']
+        fields = ["subscriptionplan", "amount"]
 
     def create(self, validated_data):
-        user = self.context['request'].user
+        user = self.context["request"].user
         payment = Payment.objects.create(
             user=user,
-            subscriptionplan=validated_data['subscriptionplan'],
-            amount=validated_data['amount'],
+            subscriptionplan=validated_data["subscriptionplan"],
+            amount=validated_data["amount"],
         )
         return payment
+
+
+class PaymentHistorySerializer(serializers.ModelSerializer):
+    subscriptionplan_type = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Payment
+        fields = [
+            "subscriptionplan_type",  # Show the type instead of the whole subscriptionplan object
+            "transaction_id",
+            "updated_at",
+            "amount",
+            "payment_status",
+        ]
+        read_only_fields = fields
+
+    def get_subscriptionplan_type(self, obj):
+        return obj.subscriptionplan.type
