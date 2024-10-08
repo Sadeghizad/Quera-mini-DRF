5ebe22c695595ad8e4fcebe3174e144d21d1cb09
Sadeghizad <mosadeghizad@gmail.com>
2024-10-08 16:48:55 +0330

    add a signal reciever to set the rating of videos

---
+++ media/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from .models import Comment
+from django.db import models
+
+@receiver(post_save, sender=Comment)
+@receiver(post_delete, sender=Comment)  # Handle when comments are deleted, too
+def update_video_rating(sender, instance, **kwargs):
+    video = instance.video
+    # Get all the comments related to this video and calculate the average rating
+    comments = Comment.objects.filter(video=video)
+    if comments.exists():
+        avg_rating = comments.aggregate(models.Avg('rate'))['rate__avg']
+        video.rating = avg_rating
+    else:
+        video.rating = None  # Reset rating if no comments
+    video.save()
