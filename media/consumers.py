31f2bd38532631f82e8a29be8f16a4d2c4f069c5
Sadeghizad <mosadeghizad@gmail.com>
2024-10-09 17:00:34 +0330

    nothing special actually

--- media/consumers.py
+++ media/consumers.py
@@ -1,49 +1,25 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+
 class VideoConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.video_id = self.scope['url_route']['kwargs']['video_id']
-        self.room_group_name = f'video_{self.video_id}'
+        self.video_id = self.scope["url_route"]["kwargs"]["video_id"]
+        self.room_group_name = f"video_{self.video_id}"
 
         # Join room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
         await self.accept()
 
     async def disconnect(self, close_code):
         # Leave room group
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
-
-    # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        action = text_data_json['action']
-
-        # Process action (e.g., 'new_comment', 'update_views')
-        # Then send the update to the room group
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'video_update',
-                'action': action,
-                'data': text_data_json['data'],
-            }
-        )
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     # Receive message from room group
     async def video_update(self, event):
-        action = event['action']
-        data = event['data']
+        action = event["action"]
+        data = event["data"]
 
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'action': action,
-            'data': data
-        }))
+        await self.send(text_data=json.dumps({"action": action, "data": data}))
--- media/signals.py
+++ media/signals.py
@@ -1,7 +1,10 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from .models import Comment,UserVideoInteraction
+from .models import Comment, UserVideoInteraction, Video
 from django.db import models
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
 
 @receiver(post_save, sender=Comment)
 @receiver(post_delete, sender=Comment)  # Handle when comments are deleted, too
@@ -10,14 +13,44 @@ def update_video_rating(sender, instance, **kwargs):
     # Get all the comments related to this video and calculate the average rating
     comments = Comment.objects.filter(video=video)
     if comments.exists():
-        avg_rating = comments.aggregate(models.Avg('rate'))['rate__avg']
+        avg_rating = comments.aggregate(models.Avg("rate"))["rate__avg"]
         video.rating = avg_rating
     else:
         video.rating = None  # Reset rating if no comments
     video.save()
 
+
 @receiver(post_save, sender=UserVideoInteraction)
-def update_video_views(sender,instance,*args, **kwargs):
+def update_video_views(sender, instance, *args, **kwargs):
     video = instance.video
     video.views = UserVideoInteraction.objects.filter(video=video).count()
     video.save()
+
+
+@receiver(post_save, sender=Video)
+def video_updated(sender, instance, **kwargs):
+    # When a video is updated, broadcast the changes via WebSocket
+    send_video_update(instance)
+
+
+def send_video_update(video):
+    """Send WebSocket update for the video."""
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        f"video_{video.id}",  # Group name based on video ID
+        {
+            "type": "video_update",
+            "action": "update_views",  # Example action
+            "data": {
+                "views": video.views,
+                "rating": str(
+                    video.rating
+                ),  # Ensure DecimalField is serialized as string
+                "title": video.title,
+                "poster": video.poster.url if video.poster else None,
+                "trailer": video.trailer,
+                "duration": video.duration,
+                "release_date": video.release_date.strftime("%Y-%m-%d"),
+            },
+        },
+    )
